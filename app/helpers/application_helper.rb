90cefc87caa3683e967d6fc4d350f44a7155fd72
Sergey <sukyans@gmail.com>
2020-08-13 23:23:51 -0400

    complete Average Rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def average_rating(reviews)
+    arr = []
+    reviews.each { |review| arr << review.rating }
+    (arr.inject{ |sum, el| sum + el }.to_f / arr.size).round(1)
+  end
 end
